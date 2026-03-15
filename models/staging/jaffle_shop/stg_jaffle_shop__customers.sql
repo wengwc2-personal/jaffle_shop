0fbd549903780ce55a1f0e94b35748e9b58f84ab
Wilson Weng <wengwc2@gmail.com>
2026-03-15 19:47:41 +0000

    clean up staging models

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,2 +1,20 @@
-select id as customer_id, first_name, last_name 
-from {{ source('jaffle_shop', 'customers') }}
+with 
+
+source as (
+
+    select * from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+renamed as (
+
+    select
+        customer_id,
+        first_name,
+        last_name
+
+    from source
+
+)
+
+select * from renamed
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,2 +1,22 @@
-select id as order_id, user_id as customer_id, order_date, status
-from {{ source('jaffle_shop', 'orders') }}
+with 
+
+source as (
+
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+renamed as (
+
+    select
+        order_id,
+        customer_id,
+        order_date,
+        status as order_status,
+        _etl_loaded_at
+
+    from source
+
+)
+
+select * from renamed
